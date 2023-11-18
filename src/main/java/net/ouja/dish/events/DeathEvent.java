9ecc9b964d136bee45aebb0c2a6c5658a3022b86
iris <60985521+KaiAF@users.noreply.github.com>
2023-11-18 16:16:16 -0600

    add EntityDeathEvent

--- src/main/java/net/ouja/dish/events/DeathEvent.java
+++ src/main/java/net/ouja/dish/events/DeathEvent.java
@@ -1,7 +1,10 @@
 package net.ouja.dish.events;
 
+import net.ouja.api.Entity;
+import net.ouja.api.Player;
 import net.ouja.api.event.EventHandler;
 import net.ouja.api.event.EventListener;
+import net.ouja.api.event.entity.EntityDeathEvent;
 import net.ouja.api.event.player.PlayerDeathEvent;
 import net.ouja.api.network.chat.Component;
 
@@ -9,8 +12,15 @@ public class DeathEvent implements EventListener {
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
-        System.out.println(event.getDeathMessage().getString());
-        System.out.println(event.getPlayer().lastDeathLocation());
         event.setDeathMessage(Component.literal("Died of Testing!"));
+    }
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        Entity entity = event.getDamageSource().getEntity();
+        if (entity instanceof Player player) {
+            // this is called only when a player killed an entity
+            System.out.println(String.format("%s killed %s", player.getName(), event.getEntity().getType().getName()));
+        }
     }
 }
